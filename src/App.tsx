6c569a6b6fb6941950c0ac6af93c72051c7385ba
dev-rahim <rahimuddin5915@gmail.com>
2023-11-16 22:01:57 +0600

    pages

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h2 className="bg-red-500">Hello world</h2>
-    </>
-  );
-}
-
-export default App;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import Home from "../page/Home";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
   },
 ]);
