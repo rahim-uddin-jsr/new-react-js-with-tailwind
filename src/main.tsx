2e15f00ca5163a988c8e3b0139539ab069439cfc
dev-rahim <rahimuddin5915@gmail.com>
2023-11-16 22:29:13 +0600

    rexux tamplate with ts and tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import { store } from "./redux/store";
 import { router } from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   </React.StrictMode>
 );
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/features/counter/counterSlice";
+import { RootState } from "../redux/store";
+
 const Home = () => {
-  return <div>Home</div>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div>
+        <div>
+          <button
+            className="bg-green-200 px-5 py-3 font-semibold text-2xl rounded-2xl"
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span className="px-5 py-3 font-semibold text-2xl">{count}</span>
+          <button
+            className="bg-yellow-200 px-5 py-3 font-semibold text-2xl rounded-2xl"
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
