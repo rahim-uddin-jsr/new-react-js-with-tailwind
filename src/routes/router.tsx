59edff37a9e7f072bd317525fa9898f05ac2047d
dev-rahim <rahimuddin5915@gmail.com>
2023-11-16 22:06:17 +0600

    not found page added

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../page/Home";
+import NotFound from "../page/NotFound";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
